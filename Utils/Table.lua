0b49f1f0190ac9a4f9d8d67acd3eb2608f2fe415
imevil1575-cmd <imevil1575@gmail.com>
2026-09-03 18:46:06 -0500

    Implement table utility functions in Table.lua
    Added utility functions for table operations including contains, find, clone, merge, and size.

--- Utils/Table.lua
+++ Utils/Table.lua
@@ -1,1 +1,40 @@
--- Table Utilities
+-- Utils/Table.lua
+local Table = {}
+
+function Table.contains(tbl, value)
+    for _, v in ipairs(tbl) do
+        if v == value then return true end
+    end
+    return false
+end
+
+function Table.find(tbl, predicate)
+    for i, v in ipairs(tbl) do
+        if predicate(v, i) then return i, v end
+    end
+    return nil
+end
+
+function Table.clone(tbl)
+    local new = {}
+    for k, v in pairs(tbl) do
+        new[k] = v
+    end
+    return new
+end
+
+function Table.merge(tbl1, tbl2)
+    local new = Table.clone(tbl1)
+    for k, v in pairs(tbl2) do
+        new[k] = v
+    end
+    return new
+end
+
+function Table.size(tbl)
+    local count = 0
+    for _ in pairs(tbl) do count = count + 1 end
+    return count
+end
+
+return Table
