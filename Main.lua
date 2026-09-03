2c9a9de087d96cc3b794bfa424c87fda565a5f67
imevil1575-cmd <imevil1575@gmail.com>
2026-09-03 18:59:50 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,11 @@
--- Main entry point for Obliterated
+-- Main.lua - Entry point for Obliterated
+-- Black & Pink UI with Goku Black theme
+-- Animation logger that learns and adapts
+
+local Init = require(script.Init)
+
+-- Run the script
+local obliterated = Init:run()
+
+-- Return for require() compatibility
+return obliterated
