f453ae100b44044953c2b3a8e1afcbc48c1ad502
imevil1575-cmd <imevil1575@gmail.com>
2026-09-03 18:47:09 -0500

    Create Action class for handling timings
    Implemented Action class with timing functionalities.

--- Timings/Action.lua
+++ Timings/Action.lua
@@ -1,1 +1,33 @@
--- Action Timings
+-- Timings/Action.lua
+local Action = {}
+Action.__index = Action
+
+function Action.new()
+    local self = setmetatable({}, Action)
+    self._type = "N/A"
+    self._when = 0
+    self.name = ""
+    self.hitbox = Vector3.zero
+    self.ihbc = false
+    return self
+end
+
+function Action:when()
+    return self._when / 1000
+end
+
+Action.TYPES = {
+    PARRY = "Parry",
+    DODGE = "Dodge",
+    START_BLOCK = "Start Block",
+    END_BLOCK = "End Block",
+    JUMP = "Jump",
+    TELEPORT_UP = "Teleport Up",
+    FORCED_FULL_DODGE = "Forced Full Dodge",
+    START_SLIDE = "Start Slide",
+    END_SLIDE = "End Slide",
+    START_CROUCH = "Start Crouch",
+    END_CROUCH = "End Crouch",
+}
+
+return Action
